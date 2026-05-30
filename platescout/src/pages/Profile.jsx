de8cd16ad9b73803faec3bea7e680280b2f0c6b5
Charles Laursen <laursec2@wwu.edu>
2026-05-30 16:16:48 -0700

    Attempting to fix 500 error, I belive it was misformatting on my fetch

--- platescout/src/pages/Profile.jsx
+++ platescout/src/pages/Profile.jsx
@@ -46,7 +46,8 @@ function Profile() {
           method: 'POST',
           headers: { 
             // Call POST /api/logout  with the header Authorization: "Bearer " + <token>
-            Authorization: 'Bearer ' + token 
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer  ${token}` 
           },
           //body: empty
         });
